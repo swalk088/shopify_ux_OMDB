4a20714b98df2e6fd15e35cd174721fa48406869
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-08 00:38:36 -0500

    added testing name

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -2,7 +2,7 @@
 
 function getOMDBApiCall(searchedItem){
     console.log(searchedItem)
-    // $.getJSON("http://www.omdbapi.com/?apikey=[e82e9afb]&s="+searchedItem).then(function(response){
-    //     console.log(response);
-    // })
+    $.getJSON("http://www.omdbapi.com/?apikey=[e82e9afb]&s="+"red").then(function(response){
+        console.log(response);
+    })
 }
