0f180f335a80b09b4af12ba2ebedcdd3eea0b633
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-08 00:43:39 -0500

    changes to made

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -2,7 +2,7 @@
 
 function getOMDBApiCall(searchedItem){
     console.log(searchedItem)
-    $.getJSON("http://www.omdbapi.com/?apikey=[e82e9afb]&s="+"red").then(function(response){
+    $.getJSON("http://www.omdbapi.com/?apikey=[e82e9afb]&t="+searchedItem).then(function(response){
         console.log(response);
     })
 }
