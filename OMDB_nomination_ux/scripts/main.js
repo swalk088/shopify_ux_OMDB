f630bc0b47d04c3433e11cc3330f2e7596aef229
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-08 00:23:43 -0500

    added searchable items

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -1,7 +1,7 @@
 
 
-function getOMDBApiCall(){
-    $.getJSON("http://www.omdbapi.com/?apikey=[e82e9afb]&").then(function(response){
+function getOMDBApiCall(searchedItem){
+    $.getJSON("http://www.omdbapi.com/?apikey=[e82e9afb]&s="+searchedItem).then(function(response){
         console.log(response);
     })
 }
