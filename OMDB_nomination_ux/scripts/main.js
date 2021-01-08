3bb5992bb743bda3bc409429c0f882a485162493
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-08 00:57:07 -0500

    added api key

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -2,7 +2,7 @@
 
 function getOMDBApiCall(searchedItem){
     console.log(searchedItem)
-    $.getJSON("http://www.omdbapi.com/?apikey=[e82e9afb]&").then(function(response){
+    $.getJSON("http://www.omdbapi.com/?apikey=[7f1de846]&").then(function(response){
         console.log(response);
     })
 }
