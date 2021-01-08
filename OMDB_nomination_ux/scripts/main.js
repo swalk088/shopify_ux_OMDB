76fcdc5c806b64f4c8716dd31bea752cee902acf
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-08 00:18:04 -0500

    changed api call

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -1,7 +1,7 @@
 
 
 function getOMDBApiCall(){
-    $.getJSON("https://www.omdbapi.com/?i=tt3896198&apikey=e82e9afb").then(function(response){
+    $.getJSON("http://www.omdbapi.com/?apikey=[e82e9afb]&").then(function(response){
         console.log(response);
     })
 }
