e93ce372b8aaa3e28f85d367d6b465365e3b0b91
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-08 00:28:22 -0500

    addded to nsubmit

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -1,6 +1,7 @@
 
 
 function getOMDBApiCall(searchedItem){
+    console.log(searchedItem)
     $.getJSON("http://www.omdbapi.com/?apikey=[e82e9afb]&s="+searchedItem).then(function(response){
         console.log(response);
     })
