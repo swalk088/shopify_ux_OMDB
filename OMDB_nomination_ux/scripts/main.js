7c29a6dbc002335f6a72c18f6dd8d9f92566e1f4
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-08 00:58:45 -0500

    added searchedable

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -2,7 +2,7 @@
 
 function getOMDBApiCall(searchedItem){
     console.log(searchedItem)
-    $.getJSON("http://www.omdbapi.com/?apikey=[7f1de846]&").then(function(response){
+    $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&t="+searchedItem).then(function(response){
         console.log(response);
     })
 }
