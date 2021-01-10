49503521e59884e6c75865c4e5dce0d0540153c0
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-10 18:36:52 -0500

    adding list to searchable

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -2,7 +2,7 @@
 var current_searched_list=[];
 
 
-function getOMDBApiCall(searchedItem){
+async function getOMDBApiCall(searchedItem){
     console.log(searchedItem)
     $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&s="+searchedItem).then(function(response){
         console.log(response);
@@ -39,14 +39,18 @@ function setMoviePicture(searchedMovie){
    
 }
 
+
 function trackSearchChanges(searchedItem) {
     // Declare variables
     var input, filter, ul, li, a, i, txtValue;
     filter = searchedItem;
     ul = document.getElementById("movieSearch");
     console.log(searchedItem);
-    getOMDBApiCall(searchedItem)
-    console.log(current_searched_list);
+    await getOMDBApiCall(searchedItem);
+    //console.log(current_searched_list);
+    for(var i=0;i<current_searched_list.length;i++){
+        console.log(current_searched_list[i]);
+    }
     // Loop through all list items, and hide those who don't match the search query
     // for (i = 0; i < li.length; i++) {
     //   a = li[i].getElementsByTagName("a")[0];
