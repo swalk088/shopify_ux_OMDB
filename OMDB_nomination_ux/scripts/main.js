3793b87c4fb398fbe78767fad078309ec0e96616
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-10 18:28:23 -0500

    added search_list global

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -1,9 +1,13 @@
 
+var current_searched_list=[];
+
 
 function getOMDBApiCall(searchedItem){
     console.log(searchedItem)
     $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&s="+searchedItem).then(function(response){
         console.log(response);
+        current_searched_list=response.search;
+
     })
     //getMoviePicture(searchedItem);
 }
@@ -38,9 +42,11 @@ function setMoviePicture(searchedMovie){
 function trackSearchChanges(searchedItem) {
     // Declare variables
     var input, filter, ul, li, a, i, txtValue;
-    filter = searchedItem.value.toUpperCase();
+    filter = searchedItem;
     ul = document.getElementById("movieSearch");
     console.log(searchedItem);
+    getOMDBApiCall(searchedItem)
+    console.log(current_searched_list);
     // Loop through all list items, and hide those who don't match the search query
     // for (i = 0; i < li.length; i++) {
     //   a = li[i].getElementsByTagName("a")[0];
