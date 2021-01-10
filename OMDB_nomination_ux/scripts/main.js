fd6711a82ca75601dd26e317428d28eccc3bbeb1
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-10 18:41:20 -0500

    moved to fetch

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -1,12 +1,14 @@
 
-var current_searched_list=[];
-
 
 async function getOMDBApiCall(searchedItem){
+    var current_searched_list=[];
     console.log(searchedItem)
     $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&s="+searchedItem).then(function(response){
-        console.log(response);
+        //console.log(response);
         current_searched_list=response.search;
+        for(var i=0;i<current_searched_list.length;i++){
+            console.log(current_searched_list[i]);
+        }
 
     })
     //getMoviePicture(searchedItem);
@@ -46,11 +48,9 @@ function trackSearchChanges(searchedItem) {
     filter = searchedItem;
     ul = document.getElementById("movieSearch");
     console.log(searchedItem);
-    await getOMDBApiCall(searchedItem);
+    getOMDBApiCall(searchedItem);
     //console.log(current_searched_list);
-    for(var i=0;i<current_searched_list.length;i++){
-        console.log(current_searched_list[i]);
-    }
+    
     // Loop through all list items, and hide those who don't match the search query
     // for (i = 0; i < li.length; i++) {
     //   a = li[i].getElementsByTagName("a")[0];
