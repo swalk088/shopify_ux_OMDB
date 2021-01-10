398c9f57d0a67b36052f2da59ad9192f2616f853
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-10 17:11:01 -0500

    added fetch for pictures

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -5,10 +5,12 @@ function getOMDBApiCall(searchedItem){
     $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&t="+searchedItem).then(function(response){
         console.log(response);
     })
+    getMoviePicture(searchedItem);
 }
 
 //using TMDB database api to get movie images to improve user experience
 function getMoviePicture(searchedMovie){
-    var main_call="https://api.themoviedb.org/3/movie/550?api_key=7c6c3f52787e7909886d3e15d34a0035/"
-
+    var main_call="https://api.themoviedb.org/3/search/movie?api_key=7c6c3f52787e7909886d3e15d34a0035&language=en-US&query="+searchedMovie;
+    fetch(main_call)
+  .then(response => console.log(response));
 }
