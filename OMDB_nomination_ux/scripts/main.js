9f3bf3ef741edff2f80e60cddf6a753ae51211c9
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-10 17:19:33 -0500

    getting movie id

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -10,7 +10,7 @@ function getOMDBApiCall(searchedItem){
 
 //using TMDB database api to get movie images to improve user experience
 function getMoviePicture(searchedMovie){
-    var main_call="https://api.themoviedb.org/3/search/movie?api_key=7c6c3f52787e7909886d3e15d34a0035&language=en-US&query="+searchedMovie;
+    var main_call="https://api.themoviedb.org/3/search/movie?api_key=7c6c3f52787e7909886d3e15d34a0035&language=en-US&query="+searchedMovie+"&pages=1&include_adult=false";
     fetch(main_call)
   .then(response => response.json())
   .then(data=>console.log(data));
