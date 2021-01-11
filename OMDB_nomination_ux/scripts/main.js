5232f777cb3e30df80473db2866671b625fb89da
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-10 22:54:54 -0500

    staged

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -22,22 +22,22 @@ async function getOMDBApiCall(searchedItem){
             //console.log(current_searched_list[i]);
             var title_response = await $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&t="+current_searched_list[i].Title)
             console.log(title_response);
-            // var li = document.createElement("li");
-            // li.setAttribute("id",current_searched_list[i].imdbID)
-            // console.log(current_searched_list[i].Title)
-            // var innerHTML="<button onclick='nominateMovie()'>Nominate</button><button type='button' class='collapsible'><img src='"+current_searched_list[i].Poster+"' width=50/> "+current_searched_list[i].Title+" ("+current_searched_list[i].Year+")</button>"
-            // innerHTML+="<div class='content'>"
-            // innerHTML+="<p>Rated: "+title_response.Rated+"</p>"
-            // innerHTML+="<p>Release: "+title_response.Release+"</p>"
-            // innerHTML+="<p>Genre: "+title_response.Genre+"</p>"
-            // innerHTML+="<p>Director: "+title_response.Director+"</p>"
-            // innerHTML+="<p>Writer: "+title_response.Writer+"</p>"
-            // innerHTML+="<p>Actors: "+title_response.Actors+"</p>"
-            // innerHTML+="<p>"+title_response.Plot+"</p>"
-            // innerHTML+="</div>";
-            // console.log(innerHTML);
-            // li.innerHTML=innerHTML;
-            // ul.appendChild(li);
+            var li = document.createElement("li");
+            li.setAttribute("id",current_searched_list[i].imdbID)
+            console.log(current_searched_list[i].Title)
+            var innerHTML="<button onclick='nominateMovie()'>Nominate</button><button type='button' class='collapsible'><img src='"+current_searched_list[i].Poster+"' width=50/> "+current_searched_list[i].Title+" ("+current_searched_list[i].Year+")</button>"
+            innerHTML+="<div class='content'>"
+            innerHTML+="<p>Rated: "+title_response.Rated+"</p>"
+            innerHTML+="<p>Release: "+title_response.Release+"</p>"
+            innerHTML+="<p>Genre: "+title_response.Genre+"</p>"
+            innerHTML+="<p>Director: "+title_response.Director+"</p>"
+            innerHTML+="<p>Writer: "+title_response.Writer+"</p>"
+            innerHTML+="<p>Actors: "+title_response.Actors+"</p>"
+            innerHTML+="<p>"+title_response.Plot+"</p>"
+            innerHTML+="</div>";
+            console.log(innerHTML);
+            li.innerHTML=innerHTML;
+            ul.appendChild(li);
         }
     }
 
@@ -48,6 +48,7 @@ async function trackSearchChanges(searchedItem) {
     // Declare variables
     // if (!currentlySearching){
         console.log(searchedItem);
+        
         await getOMDBApiCall(searchedItem);
 
         var coll = document.getElementsByClassName("collapsible");
