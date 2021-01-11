a33ada8a575249870d20fb38cd3a8e97ab23fbf0
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-11 16:45:48 -0500

    collapsible change

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -76,18 +76,16 @@ function nominateMovie(){
 function setCollapsibleOnclick(){
     var coll = document.getElementsByClassName("collapsible");
     var i;
-
+    
     for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            console.log("activated event")
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            console.log(content);
-            if (content.style.maxHeight){
-            content.style.maxHeight = null;
-            } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-            }
-        });
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
     }
 }
