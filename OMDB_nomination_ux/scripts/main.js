14e69f2fe4c3542ead8f0e6f74e96ca1a134a5ca
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-10 21:16:41 -0500

    testing log

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -2,57 +2,45 @@ var nominatedMovies=[];
 var current_searched_list=[];
 
 
-function getOMDBApiCall(searchedItem){
+async function getOMDBApiCall(searchedItem){
     console.log(searchedItem);
-    $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&s="+searchedItem).then(function(response){
-        //console.log(response);
-        current_searched_list=response.Search;
-        var ul = document.getElementById("movieSearch");        
-        while (ul.firstChild) {
-            ul.removeChild(ul.lastChild);
-        }
-        console.log(ul);
-        for(var i=0;i<current_searched_list.length;i++){
-            $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&t="+current_searched_list[i].Title).then(function(title_response){
-            console.log(title_response);  
-            var li = document.createElement("li");
-                li.setAttribute("id",current_searched_list[i].imdbID)
-                console.log(current_searched_list[i].Title)
-                var innerHTML="<button onclick'nominateMovie()'>Nominate</button><button type='button' class='collapsible'><img src='"+current_searched_list[i].Poster+"' width=50/> "+current_searched_list[i].Title+" ("+current_searched_list[i].Year+")</button>"
-                innerHTML+="<div class='content'>"
-                innerHTML+="<p>Rated: "+title_response.Rated+"</p>"
-                innerHTML+="<p>Release: "+title_response.Release+"</p>"
-                innerHTML+="<p>Genre: "+title_response.Genre+"</p>"
-                innerHTML+="<p>Director: "+title_response.Director+"</p>"
-                innerHTML+="<p>Writer: "+title_response.Writer+"</p>"
-                innerHTML+="<p>Actors: "+title_response.Actors+"</p>"
-                innerHTML+="<p>"+title_response.Plot+"</p>"
-                innerHTML+="</div>";
-                console.log(innerHTML);
-                li.innerHTML=innerHTML;
-                //console.log(li);
-                ul.appendChild(li);
-            });
+    var response = await $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&s="+searchedItem);
+    console.log(response);
+    //     //console.log(response);
+    //     current_searched_list=response.Search;
+    //     var ul = document.getElementById("movieSearch");        
+    //     while (ul.firstChild) {
+    //         ul.removeChild(ul.lastChild);
+    //     }
+    //     console.log(ul);
+    //     for(var i=0;i<current_searched_list.length;i++){
+    //         $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&t="+current_searched_list[i].Title).then(function(title_response){
+    //         console.log(title_response);  
+    //         var li = document.createElement("li");
+    //             li.setAttribute("id",current_searched_list[i].imdbID)
+    //             console.log(current_searched_list[i].Title)
+    //             var innerHTML="<button onclick='nominateMovie()'>Nominate</button><button type='button' class='collapsible'><img src='"+current_searched_list[i].Poster+"' width=50/> "+current_searched_list[i].Title+" ("+current_searched_list[i].Year+")</button>"
+    //             innerHTML+="<div class='content'>"
+    //             innerHTML+="<p>Rated: "+title_response.Rated+"</p>"
+    //             innerHTML+="<p>Release: "+title_response.Release+"</p>"
+    //             innerHTML+="<p>Genre: "+title_response.Genre+"</p>"
+    //             innerHTML+="<p>Director: "+title_response.Director+"</p>"
+    //             innerHTML+="<p>Writer: "+title_response.Writer+"</p>"
+    //             innerHTML+="<p>Actors: "+title_response.Actors+"</p>"
+    //             innerHTML+="<p>"+title_response.Plot+"</p>"
+    //             innerHTML+="</div>";
+    //             console.log(innerHTML);
+    //             li.innerHTML=innerHTML;
+    //             //console.log(li);
+    //             ul.appendChild(li);
+
+               
+
+    //         });
             
-        }
-        var coll = document.getElementsByClassName("collapsible");
-        var i;
-
-        for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-                console.log("activated event")
-                this.classList.toggle("active");
-                var content = this.nextElementSibling;
-                console.log(content);
-                if (content.style.maxHeight){
-                content.style.maxHeight = null;
-                } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-                }
-            });
-        }
-
-    });
+    //     }
+        
+    // });
 }
 
 
@@ -63,6 +51,23 @@ function trackSearchChanges(searchedItem) {
     ul = document.getElementById("movieSearch");
     console.log(searchedItem);
     getOMDBApiCall(searchedItem);
+
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            console.log("activated event")
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            console.log(content);
+            if (content.style.maxHeight){
+            content.style.maxHeight = null;
+            } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
 }
 
 function nominateMovie(nominatedMovieInfo){
