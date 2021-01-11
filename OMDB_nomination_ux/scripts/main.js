e0e028266c4af83a0a1b30fb8a48fb01405b7d3d
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-10 21:33:19 -0500

    changed to async stuff

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -3,40 +3,43 @@ var current_searched_list=[];
 
 
 async function getOMDBApiCall(searchedItem){
-    console.log(searchedItem);
-    var response = await $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&s="+searchedItem);
-    console.log(response);
-    current_searched_list=response.Search;
-    
-    
-    var ul = document.getElementById("movieSearch");        
+    var ul = document.getElementById("movieSearch"); 
+
     while (ul.firstChild) {
         ul.removeChild(ul.lastChild);
     }
 
-
+    console.log(searchedItem);
+    var response = await $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&s="+searchedItem);
+    console.log(response);
+    current_searched_list=response.Search;
+    console.log()
+    
+    
     console.log(ul);
-    for(var i=0;i<current_searched_list.length;i++){
-        var title_response = await $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&t="+current_searched_list[i].Title)
-        console.log(title_response);  
-        var li = document.createElement("li");
-        li.setAttribute("id",current_searched_list[i].imdbID)
-        console.log(current_searched_list[i].Title)
-        var innerHTML="<button onclick='nominateMovie()'>Nominate</button><button type='button' class='collapsible'><img src='"+current_searched_list[i].Poster+"' width=50/> "+current_searched_list[i].Title+" ("+current_searched_list[i].Year+")</button>"
-        innerHTML+="<div class='content'>"
-        innerHTML+="<p>Rated: "+title_response.Rated+"</p>"
-        innerHTML+="<p>Release: "+title_response.Release+"</p>"
-        innerHTML+="<p>Genre: "+title_response.Genre+"</p>"
-        innerHTML+="<p>Director: "+title_response.Director+"</p>"
-        innerHTML+="<p>Writer: "+title_response.Writer+"</p>"
-        innerHTML+="<p>Actors: "+title_response.Actors+"</p>"
-        innerHTML+="<p>"+title_response.Plot+"</p>"
-        innerHTML+="</div>";
-        console.log(innerHTML);
-        li.innerHTML=innerHTML;
-        ul.appendChild(li);
+    if (current_searched_list!==undefined){
+        for(var i=0;i<current_searched_list.length;i++){
+            var title_response = await $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&t="+current_searched_list[i].Title)
+            console.log(title_response);  
+            var li = document.createElement("li");
+            li.setAttribute("id",current_searched_list[i].imdbID)
+            console.log(current_searched_list[i].Title)
+            var innerHTML="<button onclick='nominateMovie()'>Nominate</button><button type='button' class='collapsible'><img src='"+current_searched_list[i].Poster+"' width=50/> "+current_searched_list[i].Title+" ("+current_searched_list[i].Year+")</button>"
+            innerHTML+="<div class='content'>"
+            innerHTML+="<p>Rated: "+title_response.Rated+"</p>"
+            innerHTML+="<p>Release: "+title_response.Release+"</p>"
+            innerHTML+="<p>Genre: "+title_response.Genre+"</p>"
+            innerHTML+="<p>Director: "+title_response.Director+"</p>"
+            innerHTML+="<p>Writer: "+title_response.Writer+"</p>"
+            innerHTML+="<p>Actors: "+title_response.Actors+"</p>"
+            innerHTML+="<p>"+title_response.Plot+"</p>"
+            innerHTML+="</div>";
+            console.log(innerHTML);
+            li.innerHTML=innerHTML;
+            ul.appendChild(li);
+        }
     }
-        
+
 }
 
 
