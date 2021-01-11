1b6e82a4e0c944969785826af32595afec92a823
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-10 20:36:50 -0500

    added collapsible tag

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -1,6 +1,24 @@
+var nominatedMovies=[];
+var current_searched_list=[];
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+});
+
+
 function getOMDBApiCall(searchedItem){
-    var current_searched_list=[];
-    console.log(searchedItem)
+    console.log(searchedItem);
     $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&s="+searchedItem).then(function(response){
         //console.log(response);
         current_searched_list=response.Search;
@@ -14,7 +32,7 @@ function getOMDBApiCall(searchedItem){
             var li = document.createElement("li");
             li.setAttribute("id",current_searched_list[i].imdbID)
             console.log(current_searched_list[i].Title)
-            li.innerHTML="<img src='"+current_searched_list[i].Poster+"' width=50/> "+current_searched_list[i].Title+" ("+current_searched_list[i].Year+")     <button oclick'nominateMovie("+current_searched_list[i]+")'>Nominate</button>";
+            li.innerHTML="<button oclick'nominateMovie()'>Nominate</button><button type='button' class='collapsible'><img src='"+current_searched_list[i].Poster+"' width=50/> "+current_searched_list[i].Title+" ("+current_searched_list[i].Year+")</button><div class='content'><p>Lorem ipsum...</p></div>";
             console.log(li);
             ul.appendChild(li);
         }
@@ -33,5 +51,7 @@ function trackSearchChanges(searchedItem) {
 }
 
 function nominateMovie(nominatedMovieInfo){
+    console.log(nominatedMovies);
+    console.log(current_searched_list);
     console.log(nominatedMovieInfo);
 }
