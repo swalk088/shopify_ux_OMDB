0e3f62067b05418afcd08fe2defa6fac772c0bc6
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-10 19:23:36 -0500

    got rid of tmdb

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -8,47 +8,18 @@ function getOMDBApiCall(searchedItem){
         while (ul.firstChild) {
             ul.removeChild(ul.lastChild);
         }
-        
         console.log(ul);
         for(var i=0;i<current_searched_list.length;i++){
             console.log(current_searched_list[i]);
             var li = document.createElement("li");
             li.setAttribute("id",current_searched_list[i].imdbID)
             console.log(current_searched_list[i].Title)
-            li.innerHTML="<img src='"+current_searched_list[i].Poster+"'/> "+current_searched_list[i].Title+" ("+current_searched_list[i].Year+")     <button>Nominate</button>";
+            li.innerHTML="<img src='"+current_searched_list[i].Poster+"' width=50/> "+current_searched_list[i].Title+" ("+current_searched_list[i].Year+")     <button>Nominate</button>";
             console.log(li);
             ul.appendChild(li);
         }
 
     })
-    //getMoviePicture(searchedItem);
-}
-
-//using TMDB database api to get movie images to improve user experience
-function setMoviePicture(searchedMovie){
-    var main_call="https://api.themoviedb.org/3/search/movie?api_key=7c6c3f52787e7909886d3e15d34a0035&language=en-US&query="+searchedMovie+"&pages=1&include_adult=false";
-
-    fetch(main_call)
-  .then(response => response.json())
-  .then(function(data){
-   
-    var movie_id=data.results[0].id;
-    console.log(movie_id)
-    var main_picture_call="https://api.themoviedb.org/3/movie/"+movie_id+"/images?api_key=7c6c3f52787e7909886d3e15d34a0035"
-    var poster_info=null;
-    
-    fetch(main_picture_call)
-  .then(response => response.json())
-  .then(function(data){
-    var poster_info=data.posters[0];
-    var poster_url = "https://image.tmdb.org/t/p/original/" + poster_info.file_path;
-    console.log(poster_info);
-    console.log(poster_url);
-
-    });
-    });
-   
-   
 }
 
 
