a25819232c6b7d7e645b2f341c4652ef2364838e
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-10 20:48:45 -0500

    testing console

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -7,8 +7,10 @@ var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
+        console.log("activated event")
         this.classList.toggle("active");
         var content = this.nextElementSibling;
+        console.log(content);
         if (content.style.maxHeight){
         content.style.maxHeight = null;
         } else {
