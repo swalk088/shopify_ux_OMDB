7515d5c2d62714562edd8a3e9ca2fa8740693ca8
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-10 20:39:08 -0500

    fixes

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -7,14 +7,15 @@ var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-});
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+        content.style.maxHeight = null;
+        } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
 
 
 function getOMDBApiCall(searchedItem){
