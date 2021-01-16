516307e5432c7001840d3dc443b5726e2854e662
swalk088 <43152468+swalk088@users.noreply.github.com>
2021-01-15 19:47:27 -0500

    maybe works?

--- OMDB_nomination_ux/scripts/main.js
+++ OMDB_nomination_ux/scripts/main.js
@@ -2,6 +2,40 @@ var nominatedMovies=[];
 var currentSearch="";
 var current_searched_list=[];
 
+const searchedItem = document.getElementById("searchedItem");
+
+searchedItem.addEventListener('keyup',(e)=>{
+    const searchedValue=e;
+    if(searchedValue.trim()!==currentSearch){
+        currentSearch=searchedValue;
+        console.log(searchedValue);
+        await getOMDBApiCall(searchedValue);
+        removeAllList();
+        if (current_searched_list!==undefined){
+            removeAllList();
+
+            for(var i=0;i<current_searched_list.length;i++){
+                if(document.getElementById(current_searched_list[i].imdbID)==undefined) {
+                    console.log(current_searched_list[i]);
+                    console.log(current_searched_list);
+                    var title_response = await $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&t="+current_searched_list[i].Title)
+                    //console.log(title_response);
+                    loadListItem(current_searched_list[i].imdbID,
+                        current_searched_list[i].Title,current_searched_list[i].Year,
+                        current_searched_list[i].Poster,title_response);
+                    current_searched_list[i].movieInfo=title_response;
+
+                }
+            }
+        }
+    }
+});
+
+
+
+
+
+
 
 function setModalMovie(id){
     console.log(id);
@@ -48,6 +82,8 @@ async function getOMDBApiCall(searchedItem){
     var response = await $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&s="+searchedItem+"*");
 //     console.log(response);
     current_searched_list=response.Search;
+    removeAllList();
+
     
     
 
@@ -125,28 +161,7 @@ function removeAllList(){
 
 
 async function trackSearchChanges() {
-    var searchedItem = document.getElementById("searchedItem").value;
-    if(searchedItem.trim()!==currentSearch){
-        
-        currentSearch=searchedItem;
-        console.log(searchedItem);
-        await getOMDBApiCall(searchedItem);
-        removeAllList();
-        if (current_searched_list!==undefined){
-            for(var i=0;i<current_searched_list.length;i++){
-                if(document.getElementById(current_searched_list[i].imdbID)==undefined) {
-                    //console.log(current_searched_list[i]);
-                    var title_response = await $.getJSON("http://www.omdbapi.com/?apikey=7f1de846&type=movie&t="+current_searched_list[i].Title)
-                    console.log(title_response);
-                    loadListItem(current_searched_list[i].imdbID,
-                        current_searched_list[i].Title,current_searched_list[i].Year,
-                        current_searched_list[i].Poster,title_response);
-                    current_searched_list[i].movieInfo=title_response;
-
-                }
-            }
-        }
-    }
+    
        
 }
 
